cfda7f03d4972ffbcc60c67fe34f917905a19454
Rahul tripathi <rahul.tripathi@holidaytribe.com>
2025-07-05 11:04:15 +0530

    add job applicant code

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -13,29 +13,29 @@ import {
 import { Response } from 'express';
 import { JobsService } from './jobs.service';
 import { CreateJobDto, UpdateJobDto } from './jobs.dto';
-import { ApiTags, ApiOperation, ApiBody, ApiParam,ApiResponse } from '@nestjs/swagger';
+import { ApiTags, ApiOperation, ApiBody, ApiParam, ApiResponse } from '@nestjs/swagger';
 
 @Controller('jobs')
 @ApiTags('jobs')
 export class JobsController {
-  constructor(private readonly jobsService: JobsService) {}
+  constructor(private readonly jobsService: JobsService) { }
 
   @Post("createJob")
   @ApiOperation({ summary: 'Create a new job' })
   @ApiBody({ type: CreateJobDto })
   @ApiResponse({ status: 200, description: 'Jobs created successfully' })
- @ApiResponse({ status: 201, description: 'Jobs not created' })
- @ApiResponse({ status: 500, description: 'Internal server error' })
+  @ApiResponse({ status: 201, description: 'Jobs not created' })
+  @ApiResponse({ status: 500, description: 'Internal server error' })
   async create(@Body() body: CreateJobDto, @Res() res: Response) {
     const job = await this.jobsService.createJob(body);
-    console.log(this.jobsService,'jobs')
+    console.log(this.jobsService, 'jobs')
     return res.status(HttpStatus.CREATED).json(job);
   }
   @Get("getAllJobs")
   @ApiOperation({ summary: 'Get all jobs' })
   async getAll(@Res() res: Response) {
     const jobs = await this.jobsService.getAllJobs();
-    console.log(jobs,'test')
+    console.log(jobs, 'test')
     return res.status(HttpStatus.OK).json(jobs);
   }
 
@@ -46,14 +46,14 @@ export class JobsController {
   @ApiResponse({ status: 404, description: 'Job not found' })
   @ApiResponse({ status: 500, description: 'Internal server error' })
   async getById(@Param('id') id: number, @Res() res: Response) {
-     try {
-    const job = await this.jobsService.getJobById(+id);
-    return res.status(HttpStatus.OK).json(job);
-  } catch (error) {
-    return res
-      .status(error.status || HttpStatus.INTERNAL_SERVER_ERROR)
-      .json(error.response || { message: error.message });
-  }
+    try {
+      const job = await this.jobsService.getJobById(+id);
+      return res.status(HttpStatus.OK).json(job);
+    } catch (error) {
+      return res
+        .status(error.status || HttpStatus.INTERNAL_SERVER_ERROR)
+        .json(error.response || { message: error.message });
+    }
   }
 
   @Put(':id')
@@ -71,14 +71,31 @@ export class JobsController {
     return res.status(HttpStatus.OK).json(job);
   }
 
-@Delete(':id')
-@ApiOperation({ summary: 'Delete job by ID' })
-@ApiParam({ name: 'id', type: Number })
-@ApiResponse({ status: 200, description: 'Jobs Deleted successfully' })
-@ApiResponse({ status: 404, description: 'Jobs not found' })
-@ApiResponse({ status: 500, description: 'Internal server error' })
+  @Delete(':id')
+  @ApiOperation({ summary: 'Delete job by ID' })
+  @ApiParam({ name: 'id', type: Number })
+  @ApiResponse({ status: 200, description: 'Jobs Deleted successfully' })
+  @ApiResponse({ status: 404, description: 'Jobs not found' })
+  @ApiResponse({ status: 500, description: 'Internal server error' })
   async delete(@Param('id') id: number, @Res() res: Response) {
     const job = await this.jobsService.deleteJobById(id);
     return res.status(HttpStatus.OK).json({ message: 'Job deleted', job });
+  }
+
+  @Get(':id/applicants')
+  @ApiOperation({ summary: 'Get job Application  by ID' })
+  @ApiParam({ name: 'id', type: Number })
+  @ApiResponse({ status: 200, description: 'Get All  job candidate Application  successfully' })
+  @ApiResponse({ status: 404, description: 'Job not found' })
+  @ApiResponse({ status: 500, description: 'Internal server error' })
+  async getJobApplicartionById(@Param('id') id: number, @Res() res: Response) {
+    try {
+      const job = await this.jobsService.getAllApplicantsByJobId(+id);
+      return res.status(HttpStatus.OK).json(job);
+    } catch (error) {
+      return res
+        .status(error.status || HttpStatus.INTERNAL_SERVER_ERROR)
+        .json(error.response || { message: error.message });
+    }
   }
 }
--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -1,5 +1,5 @@
 // jobs.service.ts
-import { Injectable,NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateJobDto, UpdateJobDto } from './jobs.dto';
 import { DbService } from "../db/db.service";
 import { UtilService } from "../util/util.service";
@@ -7,101 +7,121 @@ import { UtilService } from "../util/util.service";
 @Injectable()
 export class JobsService {
   private jobs: any[] = [];
-    constructor(
-      public dbService: DbService,
+  constructor(
+    public dbService: DbService,
     public utilService: UtilService,
-    ) {
+  ) {
+  }
+
+  async createJob(dto: CreateJobDto) {
+    try {
+      const setData = [
+        { set: 'job_title', value: String(dto.jobTitle) },
+        { set: 'job_code', value: String(dto.jobCode) },
+        { set: 'department', value: String(dto.department) },
+        { set: 'workplace', value: String(dto.workplace) },
+        { set: 'office_primary_location', value: String(dto.officeLocation.primary) },
+        { set: 'office_on_careers_page', value: String(dto.officeLocation.onCareersPage) },
+        {
+          set: 'office_location_additional',
+          value: dto.officeLocation.additional?.length
+            ? `{${dto.officeLocation.additional.join(',')}}`
+            : '{}',
+        },
+        { set: 'description_about', value: String(dto.description.about) },
+        { set: 'description_requirements', value: String(dto.description.requirements) },
+        { set: 'description_benefits', value: String(dto.description.benefits) },
+        { set: 'company_industry', value: String(dto.companyDetails.industry) },
+        { set: 'company_job_function', value: String(dto.companyDetails.jobFunction) },
+        { set: 'employment_type', value: String(dto.employmentDetails.employmentType) },
+        { set: 'experience', value: String(dto.employmentDetails.experience) },
+        { set: 'education', value: String(dto.employmentDetails.education) },
+        {
+          set: 'keywords',
+          value: dto.employmentDetails.keywords?.length
+            ? `{${dto.employmentDetails.keywords.join(',')}}`
+            : '{}',
+        },
+        { set: 'salary_from', value: String(dto.salary.from) },
+        { set: 'salary_to', value: String(dto.salary.to) },
+        { set: 'salary_currency', value: String(dto.salary.currency) },
+      ];
+
+      const insertion = await this.dbService.insertData('jobs', setData);
+      return this.utilService.successResponse(insertion, 'Job created successfully.');
+    } catch (error) {
+      console.error('Create Job Error:', error);
+      throw new Error('Failed to create job. Please ensure all fields are valid and meet constraints.');
     }
-
- async createJob(dto: CreateJobDto) {
-  try {
-    const setData = [
-      { set: 'job_title', value: String(dto.jobTitle) },
-      { set: 'job_code', value: String(dto.jobCode) },
-      { set: 'department', value: String(dto.department) },
-      { set: 'workplace', value: String(dto.workplace) },
-      { set: 'office_primary_location', value: String(dto.officeLocation.primary) },
-      { set: 'office_on_careers_page', value: String(dto.officeLocation.onCareersPage) },
-      {
-        set: 'office_location_additional',
-        value: dto.officeLocation.additional?.length
-          ? `{${dto.officeLocation.additional.join(',')}}`
-          : '{}',
-      },
-      { set: 'description_about', value: String(dto.description.about) },
-      { set: 'description_requirements', value: String(dto.description.requirements) },
-      { set: 'description_benefits', value: String(dto.description.benefits) },
-      { set: 'company_industry', value: String(dto.companyDetails.industry) },
-      { set: 'company_job_function', value: String(dto.companyDetails.jobFunction) },
-      { set: 'employment_type', value: String(dto.employmentDetails.employmentType) },
-      { set: 'experience', value: String(dto.employmentDetails.experience) },
-      { set: 'education', value: String(dto.employmentDetails.education) },
-      {
-        set: 'keywords',
-        value: dto.employmentDetails.keywords?.length
-          ? `{${dto.employmentDetails.keywords.join(',')}}`
-          : '{}',
-      },
-      { set: 'salary_from', value: String(dto.salary.from) },
-      { set: 'salary_to', value: String(dto.salary.to) },
-      { set: 'salary_currency', value: String(dto.salary.currency) },
-    ];
-
-    const insertion = await this.dbService.insertData('jobs', setData);
-    return this.utilService.successResponse(insertion, 'Job created successfully.');
-  } catch (error) {
-    console.error('Create Job Error:', error);
-    throw new Error('Failed to create job. Please ensure all fields are valid and meet constraints.');
-  }
-}
-
-
-
-  async getAllJobs() 
-  {
-  const query = `SELECT * FROM "jobs" ORDER BY id desc;`;
-  const result = await this.dbService.execute(query);
-   return this.utilService.successResponse(result, "Jobs list retrieved successfully.");
   }
 
-async getJobById(id: number){
-  const query = `SELECT * FROM jobs WHERE id = ${id}`;
-  const result = await this.dbService.execute(query);
-  if (!result.length) {
-    throw new NotFoundException(`Job with ID ${id} not found`);
+
+
+  async getAllJobs() {
+    const query = `SELECT * FROM "jobs" ORDER BY id desc;`;
+    const result = await this.dbService.execute(query);
+    return this.utilService.successResponse(result, "Jobs list retrieved successfully.");
   }
-  return this.utilService.successResponse(result, "Jobs  retrieved successfully.");
-}
+
+  async getJobById(id: number) {
+    const query = `SELECT * FROM jobs WHERE id = ${id}`;
+    const result = await this.dbService.execute(query);
+    if (!result.length) {
+      throw new NotFoundException(`Job with ID ${id} not found`);
+    }
+    return this.utilService.successResponse(result, "Jobs  retrieved successfully.");
+  }
 
   async updateJob(id: number, dto: UpdateJobDto) {
-      try {
-            // Convert DTO to key=value pairs for update
-            const set = Object.entries(dto).map(([key, value]) => `${key}='${value}'`);
-            const where = [`id=${id}`];
-            const updateResult = await this.dbService.updateData('jobs', set, where);
-            if (updateResult.affectedRows === 0) {
-                return this.utilService.failResponse('Job not found or no changes made.');
-            }
-            return this.utilService.successResponse(updateResult, 'Job updated successfully.');
-        } catch (error) {
-            console.error('Error updating job:', error);
-            return this.utilService.failResponse('Failed to update job.');
-        }
-  }
-async deleteJobById(id: number) {
-        try {
-            const query = `DELETE FROM "jobs" WHERE id='${id}' RETURNING *;`;
-            const result = await this.dbService.execute(query);
-            if (result.length === 0) {
-                return this.utilService.failResponse(null, "User not found or already deleted.");
-            }
-            return this.utilService.successResponse(result[0], "User deleted successfully.");
-
-        }
-        catch (error) {
-
-            console.error('Delete jobs Error:', error);
-            throw new Error(error);
-        }
+    try {
+      // Convert DTO to key=value pairs for update
+      const set = Object.entries(dto).map(([key, value]) => `${key}='${value}'`);
+      const where = [`id=${id}`];
+      const updateResult = await this.dbService.updateData('jobs', set, where);
+      if (updateResult.affectedRows === 0) {
+        return this.utilService.failResponse('Job not found or no changes made.');
+      }
+      return this.utilService.successResponse(updateResult, 'Job updated successfully.');
+    } catch (error) {
+      console.error('Error updating job:', error);
+      return this.utilService.failResponse('Failed to update job.');
     }
+  }
+  async deleteJobById(id: number) {
+    try {
+      const query = `DELETE FROM "jobs" WHERE id='${id}' RETURNING *;`;
+      const result = await this.dbService.execute(query);
+      if (result.length === 0) {
+        return this.utilService.failResponse(null, "User not found or already deleted.");
+      }
+      return this.utilService.successResponse(result[0], "User deleted successfully.");
+
+    }
+    catch (error) {
+
+      console.error('Delete jobs Error:', error);
+      throw new Error(error);
+    }
+  }
+
+  async getAllApplicantsByJobId(jobId: number) {
+    const query = `
+    SELECT 
+      c.*,
+      j.job_title AS job_title,
+      j.description_about AS job_description,
+      j.office_primary_location AS job_location 
+    FROM candidates c
+    INNER JOIN jobs j ON c.job_id = j.id
+    WHERE c.job_id = ${jobId}
+    ORDER BY c.id DESC
+  `;
+    const result = await this.dbService.execute(query);
+    if (!result.length) {
+      throw new NotFoundException(`No candidates found for Job ID ${jobId}`);
+    }
+    return this.utilService.successResponse(result, "Candidates with job details retrieved successfully.");
+  }
+
+
 }
