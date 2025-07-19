fe9c9dc29305a052817ba4bcf8d9641a0013b51f
Rahul tripathi <rahul.tripathi@holidaytribe.com>
2025-07-19 15:48:55 +0530

    implemented the linkdin functionality on nest

---
+++ src/linkedin/linkedin.controller.ts
@@ -0,0 +1,32 @@
+// src/linkedin/linkedin.controller.ts
+import { Controller, Get, Query, Res,BadRequestException  } from '@nestjs/common';
+import { LinkedinService } from './linkedin.service';
+import { Response } from 'express';
+
+@Controller('linkdin')
+export class LinkedinController {
+  constructor(private readonly linkedinService: LinkedinService) {}
+
+  @Get('auth')
+  redirectToLinkedIn(@Res() res: Response) {
+    const clientId = process.env.LINKEDIN_CLIENT_ID;
+    const redirectUri = process.env.LINKEDIN_REDIRECT_URI;
+    if (!clientId || !redirectUri) {
+    throw new BadRequestException('Missing LinkedIn OAuth configuration');
+  }
+   // const scope = 'r_liteprofile r_emailaddress w_member_social'; // Add relevant scopes for job posting
+   const scope = 'r_liteprofile'
+    const authUrl = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(
+      redirectUri,
+    )}&scope=${scope}`;
+
+    res.redirect(authUrl);
+  }
+
+  @Get('callback')
+  async handleCallback(@Query('code') code: string, @Res() res: Response) {
+    const tokenData = await this.linkedinService.getAccessToken(code);
+    // Store tokenData.access_token for later use
+    return res.json({ message: 'Token retrieved', token: tokenData });
+  }
+}
