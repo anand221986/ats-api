7571f5de6470ad656474a05165be3bce4c727b23
Rahul tripathi <rahul.tripathi@holidaytribe.com>
2025-07-20 13:47:19 +0530

    job post

--- src/linkedin/linkedin.controller.ts
+++ src/linkedin/linkedin.controller.ts
@@ -15,7 +15,7 @@ export class LinkedinController {
     throw new BadRequestException('Missing LinkedIn OAuth configuration');
   }
    // const scope = 'r_liteprofile r_emailaddress w_member_social'; // Add relevant scopes for job posting
-   const scope = 'r_liteprofile'
+   const scope = 'w_member_social'
     const authUrl = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(
       redirectUri,
     )}&scope=${scope}`;
