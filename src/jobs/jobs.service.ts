f41b399018e8cb12367faec902136901c8db259d
Rahul tripathi <rahul.tripathi@holidaytribe.com>
2025-08-14 15:45:50 +0530

    job added successfull

--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -138,8 +138,8 @@ export class JobsService {
       // const set = Object.entries(dto).map(([key, value]) => `${key}='${value}'`);
 
       const set = Object.entries(dto).map(([key, value]) => {
-       if (key === 'office_location_additional' || key === 'keywords') {
-  return `${key}=${JSON.stringify(value || {})}`;
+if (key === 'office_location_additional' || key === 'keywords') {
+  return `${key}=${(Array.isArray(value) && value.length === 0) ? '{}' : JSON.stringify(value)}`;
 }
         if (key === 'office_on_careers_page') {
           return `${key}=${value === 'true' || value === true}`;
