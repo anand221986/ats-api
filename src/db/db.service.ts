c9858a5fd07ffdc942801cfd53475f10a6b252df
anandgupta224 <anandgupta224@gmail.com>
2025-08-31 12:13:13 +0530

    db service

--- src/db/db.service.ts
+++ src/db/db.service.ts
@@ -45,9 +45,7 @@ private async runQuery(query: string, values: any[] = []): Promise<any> {
     console.error("DB Error:", error);
     throw error;
   }
-   finally {
-  this.pool.release(); // ensures the connection goes back to the pool
-}
+  
 }
 
   private prepareQuery(query: string): { query: string; values: any[] } {
@@ -84,10 +82,6 @@ private async runQuery(query: string, values: any[] = []): Promise<any> {
     console.error("Error executing query:", error);
     throw error;
   }
-    finally {
-  this.pool.release(); // ensures the connection goes back to the pool
-    }
-
 }
 
 
